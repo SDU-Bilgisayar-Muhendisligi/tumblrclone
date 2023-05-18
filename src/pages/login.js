01af14ce6fc96b20c930cc172a27c13a6405ce04
adilumutunlu <67440025+adilumutunlu@users.noreply.github.com>
2023-05-19 00:03:19 +0300

    düzenleme

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,20 +1,35 @@
 import React from 'react';
-import styled from 'styled-components'
+import styled from 'styled-components';
+import "../components/LogInForm/LogInForm.css"
   
 const LoginPage = () => {
   return (
     <div>
+      <div className="logInFormContainer">
+        <form 
+        autoComplete='off' 
+        
+        className="logInFormSection"
+        >
+          <div className='formLogo'><img  className='form-logo' src='../icons/tumblr-logo.png'></img></div>
 
-         <H1>Login Page</H1>
-
+          <label className='formLabel'>E-Posta: </label>
+          <input className='formInput' type='email' name='email' required /> 
+          &nbsp;
+          <label className='formLabel'>Şifre: </label>
+          <input className='formInput' type='password' name='password'  required />
+          <div className="buttonContainer">
+            <button className="logInBtn" type='submit'> <label className='formLabel giris'>Giriş</label>
+            &nbsp;
+            <img src={process.env.PUBLIC_URL+"/icons/right-arrow.png"} alt="right-arrow" className="rightArrow" />
+            </button>
+          </div>
+        </form>
+      </div>
+      <h1 className='error-message'></h1>
     </div>
 
   );
 };
 
-const H1 = styled.h1`
-color: white;
-
-`
-  
 export default LoginPage;
