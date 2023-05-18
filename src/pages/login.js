82f36cfe5cc9fe5a023aa8823cc2fb9e78948b4e
renescoban <renes651@gmail.com>
2023-05-19 01:34:33 +0300

    düzeltme

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,35 +1,20 @@
 import React from 'react';
-import styled from 'styled-components';
-import "../components/LogInForm/LogInForm.css"
+import styled from 'styled-components'
   
 const LoginPage = () => {
   return (
     <div>
-      <div className="logInFormContainer">
-        <form 
-        autoComplete='off' 
-        
-        className="logInFormSection"
-        >
-          <div className='formLogo'><img  className='form-logo' src='../icons/tumblr-logo.png'></img></div>
 
-          <label className='formLabel'>E-Posta: </label>
-          <input className='formInput' type='email' name='email' required /> 
-          &nbsp;
-          <label className='formLabel'>Şifre: </label>
-          <input className='formInput' type='password' name='password'  required />
-          <div className="buttonContainer">
-            <button className="logInBtn" type='submit'> <label className='formLabel giris'>Giriş</label>
-            &nbsp;
-            <img src={process.env.PUBLIC_URL+"/icons/right-arrow.png"} alt="right-arrow" className="rightArrow" />
-            </button>
-          </div>
-        </form>
-      </div>
-      <h1 className='error-message'></h1>
+         <H1>Login Page</H1>
+
     </div>
 
   );
 };
 
+const H1 = styled.h1`
+color: white;
+
+`
+  
 export default LoginPage;
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,41 +1,18 @@
-
 import React from 'react';
 import styled from 'styled-components'
   
-const SignUp = () => {
+const SignUpPage = () => {
   return (
     <div>
-      <div className="logInFormContainer">
-        <form 
-        autoComplete='off' 
-        
-        className="logInFormSection"
-        >
-          <div className='formLogo'><img  className='form-logo' src='../icons/tumblr-logo.png'></img></div>
-          <div><label className='formLabel caption'>İnternetteki köşene hoş geldin. Bi' daha sıkılmak mıkılmak yok.</label></div>
-          <div className="buttonContainer">
-
-            <button className="logInBtn goBtn" type='submit'><img src={process.env.PUBLIC_URL+"/icons/logo-google.png"} alt="right-arrow" className="label-logo" /> <label className='formLabel giris'>Google ile devam et</label>
-            &nbsp;
-            </button>
-          </div>
-          <div className="buttonContainer">
-            
-            <button className="logInBtn goBtn" type='submit'><img src={process.env.PUBLIC_URL+"/icons/logo-apple.png"} alt="right-arrow" className="label-logo" /> <label className='formLabel giris'>Apple ile devam et</label>
-            &nbsp;
-            </button>
-          </div>
-          <div className="buttonContainer">
-            
-            <button className="logInBtn goBtn" type='submit'><img src={process.env.PUBLIC_URL+"/icons/logo-mail.png"} alt="right-arrow" className="label-logo" /> <label className='formLabel giris'>E-posta ile devam et</label>
-            &nbsp;
-            </button>
-          </div>
-          <div ><label className='formLabel twitter-caption'>Twitter'dan mı geliyorsun? Hadi kaydol</label></div>
-        </form>
-      </div>
+        <H1>Sign Up Page</H1>
     </div>
 
   );
 };
-export default SignUp;
+
+const H1 = styled.h1`
+color: white;
+
+`
+  
+export default SignUpPage;
